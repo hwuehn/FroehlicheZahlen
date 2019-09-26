a9d7a2398c1b668d8ea047eda60c1349ac728cbe
Henning Wuehn <hwuehn@local>
2019-09-26 08:59:30 +0200

    Feature: added classes and methods

---
+++ com/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import data.Zahlen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Zahlen isHappy = new Zahlen();
+        isHappy.test(19);
+    }
+
+
+
+
+}
---
+++ com/data/Zahlen.java
@@ -0,0 +1,44 @@
+package data;
+
+public class Zahlen {
+
+    public int quersumme(int number) {
+        String numberString = numberToString(number);
+        int sum = 0;
+        int squareNumber = 1;
+        for ( int i = 0 ; i < numberString.length( ) ; i++ ) {
+            squareNumber = square(Integer.parseInt(numberString.substring(i, i + 1)));
+            sum += squareNumber;
+
+        }
+        return sum;
+    }
+
+    public void test(int number) {
+        String numberString = numberToString(number);
+        for ( int i = 0 ; i < numberString.length( ) ; i++ ) {
+            System.out.print ( numberString.substring( i , i + 1 ) ) ;
+            System.out.print("^2");
+            if ( i == numberString.length( ) - 1 ) {
+                System.out.print( " = " ) ;
+                System.out.println( quersumme( number ) ) ;
+            }
+            else {
+                System.out.print( " + " ) ;
+            }
+
+
+        }
+    }
+
+    public String numberToString(int number) {
+        String numberstring = String.valueOf(number);
+        return numberstring;
+    }
+
+    public int square(int number) {
+        int squareNumber = (int) Math.pow(number, 2);
+        return squareNumber;
+    }
+
+}
